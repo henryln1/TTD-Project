7c6d7164a54f405a3961ac09c1fe7754218f61b3
Henry Lin <v-henry.lin@thetradedesk.com>
2018-07-13 12:21:00 -0700

    updated url logic to check for soft 404s

--- code/check_url.py
+++ code/check_url.py
@@ -1,4 +1,32 @@
 import requests
+import re
+
+
+def extensive_check_for_ads_txt(request):
+	'''
+	After performing the initial status code check, we now need to check for soft 404s and other annoying things
+	to verify whether or not there is a ads.txt file present. Boolean function
+
+	'''
+	content = request.text
+
+	if ((
+		'<!DOCTYPE' in content or 
+		'<!doctype' in content or 
+		'<html' in content or 
+		'<HTML' in content or 
+		'<content:encoded' in content) 
+	and
+		('DIRECT' in content or 
+		'RESELLER' in content or 
+		'direct' in content or 
+		'reseller' in content)
+	and (not re.search('Page not found', content, re.IGNORECASE))):
+		return True
+
+
+	return False
+
 
 
 def check_valid_url_ad_txt(url_path):
@@ -7,6 +35,6 @@ def check_valid_url_ad_txt(url_path):
 	'''
 	request = requests.get(url_path)
 	if request.status_code == 200:
-		return True
+		return extensive_check_for_ads_txt(request)
 	return False
 
---
+++ code/utils.py
@@ -0,0 +1,4 @@
+
+
+def remove_duplicates_from_list(list_):
+	return list(set(list_))
