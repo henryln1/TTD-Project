a0600acd883e4c752efd49729ddc4bda4a83e353
Henry Lin <v-henry.lin@thetradedesk.com>
2018-07-18 14:12:25 -0700

    started putting everything together in main.py, updated README with current TODOs

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 '''
@@ -8,3 +8,12 @@ Place to keep functions that may be generally useful in the future
 
 def remove_duplicates_from_list(list_):
 	return list(set(list_))
+
+
+def validate_file(file):
+	'''
+	Checks to make sure a file is valid and readable
+	'''
+	if os.path.isfile(file) and os.access(file, os.R_OK):
+		return True
+	return False
