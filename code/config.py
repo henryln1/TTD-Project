69c8c13a7d78e992de24b150f2122dd05093e866
Henry Lin <v-henry.lin@thetradedesk.com>
2018-07-26 15:01:33 -0700

    end of day 07/26/18, created config.py

---
+++ code/config.py
@@ -0,0 +1,25 @@
+'''
+Contains the important variables needed across multiple python files
+
+
+'''
+
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb', endpoint_url = 'http://localhost:8000/')
+
+
+possible_app_stores = ['Apple', 'Google', 'Tencent']
+
+app_store_to_csv_dict = {
+	'Apple': '../apple_apps.csv',
+	'Google': '../google_apps.csv',
+	'Tencent': '../tencent_apps.csv'
+}
+
+
+store_keywords_dict =  {
+		'Google': ('title', 'market_url', 'website', 'package_name'),
+		'Apple': ('artistName', 'artistViewUrl', 'sellerUrl', 'bundleId')
+}
