46e02a6f5c4a68ef582890ffde1ae0b07920bf4d
Henry Lin <v-henry.lin@thetradedesk.com>
2018-08-29 16:09:13 -0700

    attempting to upload to ttd-test, updated zip script

--- package_for_lambda.ps1
+++ package_for_lambda.ps1
@@ -3,8 +3,6 @@
 
 
 #removes pre-existing copies
-rm project-lambda-package.zip
-
 
 mkdir package-project-dir
 rm -r code/*pycache*
@@ -39,6 +37,6 @@ cp -r C:/Users/v-henry.lin/AppData/Local/Programs/Python/Python37/lib/site-packa
 
 
 #zip -r project-lambda-package package-project-dir
-Compress-Archive -Path package-project-dir/* -CompressionLevel Optimal -DestinationPath project-lambda-package.zip
+Compress-Archive -Force -Path package-project-dir/* -CompressionLevel Optimal -DestinationPath project-lambda-package.zip
 
 rm -r package-project-dir
